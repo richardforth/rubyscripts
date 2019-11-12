45712874cf2842b6a8d6785b7c5f47715ca84eb8
Richard Forth <richard.forth@gmail.com>
2019-11-12 11:55:40 +0000

    bug fix
    bugfix

--- fasttrack.rb
+++ fasttrack.rb
@@ -7,5 +7,10 @@ date_now = Time.now
 diff = ((date_fast_ends - date_now) / 3600).round(2)
 diff = diff.to_s.split('.')
 hours = diff[0]
-minutes = diff[1]
+# Current bug .75 means 75% or 45 minutes, currently printing 75 minutes
+# convert eg 75 to float (75.0)
+p diff[1]
+minutes = 60 * (diff[1].to_f / 100) # change to .75 and back to string literal
+minutes = minutes.floor.round(0)
+minutes = minutes.to_i
 puts "Your current fast ends in #{hours} hours and #{minutes} minutes."
