429e2c63f3d2a003bcea02054c0ee7f5de24e8d5
Richard Forth <richard.forth@gmail.com>
2019-11-17 09:18:54 +0000

    Create awesome_print_heredocs_testing.rb
    awesome_print with heredocs, as a test

---
+++ awesome_print_heredocs_testing.rb
@@ -0,0 +1,22 @@
+require 'awesome_print'
+
+puts <<EOF.cyan
+  This section of
+  text should appear
+  in the colour
+  CYAN.
+EOF
+
+puts <<EOF.yellowish
+  This section of
+  text should appear
+  in the colour
+  YELLOWISH.
+EOF
+
+puts <<EOF.red
+  This section of
+  text should appear
+  in the colour
+  RED.
+EOF
