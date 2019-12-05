252863507e3173133031e961baf371eb4e2b6c1a
Richard Forth <richard.forth@gmail.com>
2019-12-05 07:23:49 +0000

    testing the load method

---
+++ load/loadee.rb
@@ -0,0 +1,1 @@
+puts "> this line is coming from loadee.rb"
---
+++ load/loader.rb
@@ -0,0 +1,8 @@
+puts "This file"
+load 'loadee.rb'
+puts "And back to this file again"
+
+# H:\Documents\GitHub\rubyscripts\load>ruby loader.rb
+# This file
+# > this line is coming from loadee.rb
+# And back to this file again
